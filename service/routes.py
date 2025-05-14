358dfa03fcb6efd3de1945a239f8ddbc448f7546
Mtesby <120510058+Mtesby@users.noreply.github.com>
2025-05-14 14:44:42 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -79,6 +80,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -90,6 +92,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
 
     return account.serialize(), status.HTTP_200_OK
 
@@ -123,6 +126,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
